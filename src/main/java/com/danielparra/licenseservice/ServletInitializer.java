f6f4c7ed93c9250b7fac4d3a962b47ec78eec9fd
Daniel Parra <dani.parra.90@gmail.com>
2019-10-26 18:38:40 +0200

    Initial project

---
+++ src/main/java/com/danielparra/licenseservice/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.danielparra.licenseservice;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(LicenseServiceApplication.class);
+	}
+
+}
