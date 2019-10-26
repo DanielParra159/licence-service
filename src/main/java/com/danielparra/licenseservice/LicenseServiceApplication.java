0342fd927ea95ad30154eb978b140b8aa92c93f9
Daniel Parra <dani.parra.90@gmail.com>
2019-10-27 01:02:02 +0200

    Register on eureka discovery service

--- src/main/java/com/danielparra/licenseservice/LicenseServiceApplication.java
+++ src/main/java/com/danielparra/licenseservice/LicenseServiceApplication.java
@@ -3,9 +3,11 @@ package com.danielparra.licenseservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @RefreshScope
+@EnableEurekaClient
 public class LicenseServiceApplication {
 
 	public static void main(String[] args) {
